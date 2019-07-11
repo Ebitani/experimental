376f2e8210152cefeb1564262d6ddf7ac3aa848f
Ebitani <s1260192@u-aizu.ac.jp>
2019-07-11 16:20:16 +0900

    Initial revirsion

---
+++ DiceGame.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  int D1, D2;
+  int D;
+
+  D1 = rand()%6 +1;
+  D2 = rand()%6 +1;
+  D = D1 + D2;
+
+
+  printf("Rolling the dice...\n");
+  printf("Die 1: %d\n",D1);
+  printf("Die 2: %d\n",D2);
+  printf("Total value: %d\n",D);
+
+  return 0;
+}
